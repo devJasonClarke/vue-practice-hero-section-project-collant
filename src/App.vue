bb12606c9715546d7fced7ed9a9cf27fecf42f5a
Jettison200 <jettison200@gmail.com>
2020-10-24 19:29:37 -0500

    did stuff

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<template >
+<template>
   <div class="main-container">
-<Nav />
-<Header />
+    <Nav />
+    <Header />
+    <HowItWorks />
   </div>
 </template>
 
 <script>
-import Nav from './components/nav/nav';
-import Header from './components/header/header';
-  export default {
-    name: 'App',
-components: {
-  Nav, Header
-},
-  }
+import Nav from "./components/nav/nav";
+import Header from "./components/header/header";
+import HowItWorks from './components/how-it-works/sec-1';
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Header,
+    HowItWorks
+  },
+};
 </script>
 
-<style lang="scss"  src="./assets/scss/index.scss" >
-
-</style>
+<style lang="scss" src="./assets/scss/index.scss"></style>
---
+++ src/components/how-it-works/sec-1.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <h2>Collant stands behind some of the greatest brands</h2>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'HowItWorks'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
